9fd8db9a7e5e48a1a0f5aff8d88d905316ca5616
Roy Tang <roytang.rb@gmail.com>
2022-05-21 01:33:19 +0800

    add files and links

---
+++ src/tarjan_bridge_finding.py
@@ -0,0 +1,9 @@
+"""Tarjan's algorithm for find bridges in graph
+
+https://leetcode.com/problems/critical-connections-in-a-network/
+
+Definition of bridge:
+Deletion of a bridge increases the graph's number of connected components
+"""
+
+# ref: https://cp-algorithms.com/graph/bridge-searching.html#algorithm
