3331b6917168a7fb54e0c77e0f233657f6e0ba7d
Roytangrb <roytang.rb@gmail.com>
2022-07-10 22:34:50 +0800

    add 1696 space compressed approach

--- leetcode/python/1696-jump-game-vi.py
+++ leetcode/python/1696-jump-game-vi.py
@@ -6,7 +6,7 @@ from collections import deque
 
 
 class Solution:
-    def maxResult(self, nums: list[int], k: int) -> int:
+    def maxResult__mono_queue(self, nums: list[int], k: int) -> int:
         n = len(nums)
         dp = [0] * n
         # use monotonic deque to keep track of max values in sliding window
@@ -24,3 +24,23 @@ class Solution:
             window.append(i)
 
         return dp[-1]
+
+    def maxResult__mono_queue_compressed(self, nums: list[int], k: int) -> int:
+        """O(k) memory, at each step only previous score within the window needs
+        to be considered"""
+        n = len(nums)
+        # (sum of score, index used)
+        window: deque[tuple[int, int]] = deque([(nums[0], 0)])
+
+        for i in range(1, n):
+            while window[0][1] < i - k:
+                window.popleft()
+
+            curr = window[0][0] + nums[i]
+
+            while window and window[-1][0] <= curr:
+                window.pop()
+
+            window.append((curr, i))
+
+        return window[-1][0]
