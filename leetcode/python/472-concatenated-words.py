5568e0aa6eaafc8b4a754c9f533fa3ca34b75cbc
Roytangrb <roytang.rb@gmail.com>
2023-01-27 15:05:02 +0800

    feat: add lc daily 472 dp

--- leetcode/python/472-concatenated-words.py
+++ leetcode/python/472-concatenated-words.py
@@ -1,3 +1,27 @@
 # Author: RT
 # Date: 2023-01-27T05:08:09.162Z
 # URL: https://leetcode.com/problems/concatenated-words/
+
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: list[str]) -> list[str]:
+        ans = []
+        ws = frozenset(words)
+
+        for word in words:
+            n = len(word)
+            dp = [False] * (n + 1)
+            dp[0] = True
+            for i in range(1, n + 1):
+                dp[i] = any(
+                    dp[j] and word[j:i] in ws
+                    for j in range(
+                        0 if i != n else 1,  # exclude the current word itself
+                        i,
+                    )
+                )
+
+            if dp[n]:
+                ans.append(word)
+
+        return ans
