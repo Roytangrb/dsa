8c99aaa92316b6ea54d6ce2ff6917f2cb78b9b73
Roytangrb <roytang.rb@gmail.com>
2023-03-21 21:55:07 +0800

    feat: add lc daily 2348 sliding window

---
+++ leetcode/python/2348-number-of-zero-filled-subarrays.py
@@ -0,0 +1,18 @@
+# Author: RT
+# Date: 2023-03-21T13:54:01.783Z
+# URL: https://leetcode.com/problems/number-of-zero-filled-subarrays/
+
+
+from itertools import chain
+
+
+class Solution:
+    def zeroFilledSubarray(self, nums: list[int]) -> int:
+        l = ans = 0
+        for r, num in enumerate(chain(nums, [1])):
+            if num != 0:
+                k = r - l
+                ans += (k + 1) * k // 2
+                l = r + 1
+            
+        return ans
